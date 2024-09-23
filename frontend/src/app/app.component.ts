0299d6e9df2bbe985115b037adae1987f1270477
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-23 14:32:32 -0400

    Add some color overrides to match Figma design

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [
+    MatButton
+  ],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
