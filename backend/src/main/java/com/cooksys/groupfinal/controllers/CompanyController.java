e311728a3548caf8c440bfc762a2a8620488a290
Sylrelessa <briana.oneal823@gmail.com>
2024-09-23 16:14:59 -0400

    post user endpoint

---
+++ backend/src/main/java/com/cooksys/groupfinal/controllers/CompanyController.java
@@ -0,0 +1,44 @@
+package com.cooksys.groupfinal.controllers;
+
+import java.util.Set;
+
+import com.cooksys.groupfinal.dtos.*;
+import org.springframework.web.bind.annotation.*;
+
+import com.cooksys.groupfinal.services.CompanyService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/company")
+@RequiredArgsConstructor
+public class CompanyController {
+	
+	private final CompanyService companyService;
+	
+	@GetMapping("/{id}/users")
+    public Set<FullUserDto> getAllUsers(@PathVariable Long id) {
+        return companyService.getAllUsers(id);
+    }
+	
+	@GetMapping("/{id}/announcements")
+    public Set<AnnouncementDto> getAllAnnouncements(@PathVariable Long id) {
+        return companyService.getAllAnnouncements(id);
+    }
+	
+	@GetMapping("/{id}/teams")
+    public Set<TeamDto> getAllTeams(@PathVariable Long id) {
+        return companyService.getAllTeams(id);
+    }
+	
+	@GetMapping("/{companyId}/teams/{teamId}/projects") 
+	public Set<ProjectDto> getAllProjects(@PathVariable Long companyId, @PathVariable Long teamId) {
+		return companyService.getAllProjects(companyId, teamId);
+	}
+
+    //check the url path****
+    @PostMapping("/{id}/users")
+    public FullUserDto addUser(@PathVariable Long id, @RequestBody UserRequestDto userRequestDto) {
+        return companyService.addUser(id, userRequestDto);
+    }
+}
