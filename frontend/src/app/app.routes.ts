317c85629583d7cbb5bdfd50bdfedacd63c183ec
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-24 00:02:07 -0400

    Add responsive nav-bar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import {AppComponent} from "./app.component";
 import {Routes} from "@angular/router";
+import {LayoutComponent} from "./shared/layout/layout.component";
+import {LoginComponent} from "./login/login.component";
 
 export const routes: Routes = [
   { path: "", component: AppComponent },
   {
     path: "app",
-    component: AppComponent,
+    component: LayoutComponent,
     children: [
-      { path: "login", component: AppComponent },
+      { path: "login", component: LoginComponent },
       { path: "company", component: AppComponent },
       { path: "home", component: AppComponent },
       { path: "teams", component: AppComponent },
