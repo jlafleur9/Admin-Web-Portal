5fd997a407b8764ef24148dcbfede6fa0e92e0cf
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-24 11:38:52 -0400

    Add full skeleton and link routing

---
+++ frontend/src/app/projects/create-project-overlay/create-project-overlay.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-project-overlay',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-project-overlay.component.html',
+  styleUrl: './create-project-overlay.component.css'
+})
+export class CreateProjectOverlayComponent {
+
+}
---
+++ frontend/src/app/teams/create-team-overlay/create-team-overlay.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-team-overlay',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-team-overlay.component.html',
+  styleUrl: './create-team-overlay.component.css'
+})
+export class CreateTeamOverlayComponent {
+
+}
