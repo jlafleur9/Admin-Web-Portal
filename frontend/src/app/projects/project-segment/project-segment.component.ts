b11f7656b68165297094cfbf74b55cf6f221f28e
Sylrelessa <briana.oneal823@gmail.com>
2024-09-24 15:22:59 -0400

    finished projects layout

--- frontend/src/app/projects/project-segment/project-segment.component.ts
+++ frontend/src/app/projects/project-segment/project-segment.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-segment',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-segment.component.html',
   styleUrl: './project-segment.component.css'
 })
 export class ProjectSegmentComponent {
-
+  active = true;
 }
