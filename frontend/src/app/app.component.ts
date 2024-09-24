65be5c1cf1321fb7b7c55bc32a1ceed8f2119b29
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-23 20:58:49 -0400

    Add login page and service

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {MatButton} from "@angular/material/button";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   imports: [
-    MatButton
+    MatButton,
+    RouterOutlet,
   ],
   styleUrls: ['./app.component.css']
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,18 +1,21 @@
 import {AppComponent} from "./app.component";
 import {Routes} from "@angular/router";
+import {LoginComponent} from "./login/login.component";
+import {IsNotAuthenticated} from "../routingGuards/is-not-authenticated.guard";
+import {IsAuthenticated} from "../routingGuards/is-authenticated.guard";
 
 export const routes: Routes = [
-  { path: "", component: AppComponent },
+  { path: "login", component: LoginComponent, canActivate: [IsNotAuthenticated] },
   {
     path: "app",
     component: AppComponent,
+    canActivate: [IsAuthenticated],
     children: [
-      { path: "login", component: AppComponent },
       { path: "company", component: AppComponent },
       { path: "home", component: AppComponent },
       { path: "teams", component: AppComponent },
       { path: "users", component: AppComponent },
-      { path: "**", redirectTo: "home", pathMatch: "full" }
     ]
-  }
+  },
+  { path: "**", redirectTo: "login", pathMatch: "full" },
 ];
